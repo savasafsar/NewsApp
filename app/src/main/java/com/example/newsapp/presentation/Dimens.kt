c2120a4ade34d35c92b05ad231583bfc6740e0e7
savasafsar <savasafsar3443@gmail.com>
2023-12-03 06:42:56 +0300

    ShimmerEffectAdd

--- app/src/main/java/com/example/newsapp/presentation/Dimens.kt
+++ app/src/main/java/com/example/newsapp/presentation/Dimens.kt
@@ -4,12 +4,20 @@ import androidx.compose.ui.unit.dp
 
 object Dimens {
     val MediumPadding1 = 24.dp
+
     val MediumPadding2 = 30.dp
+
     val MediumPadding3 = 24.dp
+
     val IndicatorSize = 14.dp
-    val PageIndicatorWidth= 52.dp
-    val ArticleCardSize= 96.dp
-    val ExtraSmallPadding= 3.dp
-    val ExtraSmallPadding2= 6.dp
-    val SmallIconSize= 11.dp
+
+    val PageIndicatorWidth = 52.dp
+
+    val ArticleCardSize = 96.dp
+
+    val ExtraSmallPadding = 3.dp
+
+    val ExtraSmallPadding2 = 6.dp
+
+    val SmallIconSize = 11.dp
 }
